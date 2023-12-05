97157962977f5511bcdabbb2a42bcbff98858aa5
jbird11801@gmail.com <jbird11801@gmail.com>
2023-12-05 02:19:39 -0500

    feature/server

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -9,7 +9,7 @@ const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-
+  //usemutations (LOGIN_USER)
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
